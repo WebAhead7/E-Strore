8f8deb8bf09621bc650cbf413891b463f8904684
mj majadly <majadly@nevada.unr.edu>
2020-11-25 22:33:35 +0200

    upd2

--- src/components/RepoList/index.jsx
+++ src/components/RepoList/index.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import data from "../../data";
+import React from 'react';
+import data from '../../data/data';
 
 const RepoList = ({ priceFilter, catFilter }) => {
   const [minPrice, maxPrice] = priceFilter;
   const storeItems = data
     .filter((item) => item.price >= minPrice && item.price <= maxPrice)
-    .filter((item) => catFilter === "all" || catFilter === item.category)
+    .filter((item) => catFilter === 'all' || catFilter === item.category)
     .map((item) => (
-      <li key={item.id} className="card">
+      <li key={item.id} className='card'>
         <h3>{item.title}</h3>
-        <img src={item.image} alt="" className="center"></img>
+        <img src={item.image} alt='' className='center'></img>
         <div>${item.price.toFixed(2)}</div>
         <button>Add to Cart</button>
       </li>
     ));
   return (
-    <ul className="grid">
+    <ul className='grid'>
       {storeItems.length ? (
         storeItems
       ) : (
-        <li className="card">No results found</li>
+        <li className='card'>No results found</li>
       )}
     </ul>
   );
